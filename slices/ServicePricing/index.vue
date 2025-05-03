87a2e077fd049ca703db5e8a1055812c4f2716cd
JosBouma <jos@loodzwaar-media.nl>
2025-05-03 18:14:16 +0200

    Tarieven opmaak

--- slices/ServicePricing/index.vue
+++ slices/ServicePricing/index.vue
@@ -14,98 +14,28 @@ defineProps(
 
 function formatCurrenycy(val: any): string {
   const amount = parseFloat(val);
-  if(amount === Math.round(amount)) {
+  if (amount === Math.round(amount)) {
     return `${amount},-`;
   }
   return val.toFixed(2).replace('.', ',');
 }
 </script>
 
-<style>
-.service-pricing {
-  margin-bottom: 6rem;
-}
-
-.service-pricing .container {
-  max-width: 70rem;
-  padding: 0 1rem;
-  margin: 0 auto;
-  display: grid;
-  gap: 4rem;
-}
-
-.service-pricing .container .prices {
-  display: grid;
-  grid-template-columns: 1fr auto;
-  margin: 0 6vw;
-  row-gap: 1rem;
-  align-items: end;
-}
-
-.service-pricing .container .prices > span {
-  border-bottom: 3px solid #000;
-  padding: 1rem 0;
-}
-
-.service-pricing .container .prices > .content {
-  display: grid;
-  grid-template-columns: 1fr 20fr;
-  border-bottom: 3px solid var(--color-primary);
-}
-
-.service-pricing .container .prices > .content p {
-  font-weight: 500;
-  padding: 1rem 0;
-}
-
-.service-pricing .container .prices > .content svg {
-  color: var(--color-primary);
-  margin-top: 1.3rem;
-}
-
-.service-pricing .container .bottom .footnote {
-  margin-bottom: 2rem;
-}
-
-.service-pricing .container .bottom .footnote p {
-  font-size: 0.8rem;
-  max-width: 40rem;
-  margin: 0 auto;
-}
-
-.service-pricing .container .bottom .content {
-  max-width: 50rem;
-  margin: 0 auto;;
-}
-
-.service-pricing .container .bottom .content h3 {
-  margin-bottom: 2rem;
-}
-
-.service-pricing .container .bottom .content p {
-  margin-bottom: 1rem;
-}
-</style>
-
 <template>
-  <section class="service-pricing">
-    <div class="container">
-      <h1>{{ slice.primary.heading }}</h1>
-      <div class="prices">
-        <template v-for="item in slice.items">
-          <div class="content">
-            <svgo-chevron></svgo-chevron>
-            <prismic-rich-text :field="item.content"></prismic-rich-text>
-          </div>
-          <span>&nbsp;€ {{ formatCurrenycy(item.price) }}</span>
-        </template>
+  <div class="max-w-[50rem] mx-auto px-4 grid gap-8">
+    <h2 class="text-3xl font-bold text-center">{{ slice.primary.heading }}</h2>
+    <div v-for="item in slice.items" class="grid lg:grid-cols-[2fr_1fr]">
+      <div class="grid grid-cols-[2rem_1fr] items-center px-8 border-b-2 gap-4 text-xl">
+        <svgo-chevron :fontControlled="false" class="text-shade-1"></svgo-chevron>
+        <prismic-rich-text :field="item.content"></prismic-rich-text>
       </div>
-      <div class="bottom">
-        <prismic-rich-text :field="slice.primary.footnote" class="footnote"></prismic-rich-text>
-        <div class="content">
-          <prismic-rich-text :field="slice.primary.content"></prismic-rich-text>
-        </div>
+      <span class="border-b-2 border-black text-center mt-auto">&nbsp;€ {{ formatCurrenycy(item.price) }}</span>
+    </div>
+    <div class="max-w-[40rem] mx-auto">
+      <prismic-rich-text :field="slice.primary.footnote"></prismic-rich-text>
+      <div class="">
+        <prismic-rich-text :field="slice.primary.content"></prismic-rich-text>
       </div>
     </div>
-  </section>
+  </div>
 </template>
