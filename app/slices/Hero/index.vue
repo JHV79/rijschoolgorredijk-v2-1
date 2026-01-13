eab5fd5fcf639b5e30d482171549b0d900ea0eb0
Jos Bouma <josjjbouma@gmail.com>
2026-01-13 17:48:15 +0100

    LCP optimalisatie

--- app/slices/Hero/index.vue
+++ app/slices/Hero/index.vue
@@ -67,29 +67,9 @@ onBeforeUnmount(() => {
     <div class="2xl:max-w-4xl 2xl:mr-auto grid items-center" ref="parent">
       <div class="col-start-1 row-start-1 transition-all duration-[2s]"
         :class="[current === index ? 'opacity-100' : 'opacity-0']" v-for="(img, index) in imgs" :key="img.url">
-        <nuxt-img class="w-full h-auto rounded-lg" :src="img.url" :alt="img.alt" :width="img.width" :height="img.height"
+        <nuxt-img sizes="100vw sm:640px md:758px lg:1024px" class="w-full h-auto rounded-lg" :src="img.url" :alt="img.alt" :width="img.width" :height="img.height"
           fit="contain" />
       </div>
     </div>
   </div>
-  <!-- <div class="mx-auto max-w-[100rem] flex flex-wrap justify-center py-16 2xl:gap-8">
-    <div class="p-8 max-w-[40rem]"> 
-      <h1 class="text-4xl font-bold text-shade-1 mb-8 drop-shadow-lg">{{ slice.primary.heading_1 }}</h1>
-      <div class="px-4 py-8 lg:px-12">
-        <h2 v-if="slice.primary.heading_2" class="text-2xl mb-8">{{ slice.primary.heading_2 }}</h2>
-        <nuxt-link v-if="slice.primary.cta_link?.url" :to="asLink(slice.primary.cta_link) || '#unresolved'" class="bg-shade-1 inline-block text-center text-2xl text-white p-4 lg:px-12 rounded-lg">{{ slice.primary.cta_text }}</nuxt-link>
-      </div>
-    </div>
-    <div class="grid overflow-hidden" ref="parent">
-      <div class="col-start-1 row-start-1 transition-all duration-[3s]" v-for="(item, idx) in slice.items" :class="{ '-translate-x-[20vw] -translate-y-40 opacity-0': current !== idx }">
-        <nuxt-img
-          class="rounded-3xl max-w-2xl 2xl:max-w-none"
-          :src="item.image.url || ''"
-          width="844"
-          height="770"
-          fit="contain"
-        />
-      </div>
-    </div>
-  </div> -->
 </template>
